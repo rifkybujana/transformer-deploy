74d8dc4cb3485cad7098221e3b6429673a7aef64
Rifky Bujana Bisri <rifkybujanabisri@gmail.com>
2023-05-03 18:57:43 -0700

    add arguments on the qa example

--- demo/question-answering/triton_client.py
+++ demo/question-answering/triton_client.py
@@ -26,13 +26,13 @@ if __name__ == "__main__":
     )
     parser.add_argument("--length", required=True, help="sequence length", choices=(16, 256), type=int)
     parser.add_argument("--model", required=True, help="model type", choices=("onnx", "tensorrt"))
+    parser.add_argument("--url", default="127.0.0.1:8000", help="REST API endpoint. Default is localhost:8000", type=str)
+    parser.add_argument("--model-version", default="1", help="model version", type=str)
+    parser.add_argument("--batch-size", default=1, help="batch size", type=int)
     args, _ = parser.parse_known_args()
 
     setup_logging()
     model_name = f"transformer_{args.model}_inference"
-    url = "127.0.0.1:8000"
-    model_version = "1"
-    batch_size = 1
 
     if args.length == 256:
         question = "Which name is also used to describe the Amazon rainforest in English?"
@@ -51,23 +51,23 @@ if __name__ == "__main__":
         question = "Where do I live?"
         text = "My name is Wolfgang and I live in Berlin"
 
-    triton_client = tritonclient.http.InferenceServerClient(url=url, verbose=False)
+    triton_client = tritonclient.http.InferenceServerClient(url=args.url, verbose=False)
     assert triton_client.is_model_ready(
-        model_name=model_name, model_version=model_version
+        model_name=model_name, model_version=args.model_version
     ), f"model {model_name} not yet ready"
 
-    model_metadata = triton_client.get_model_metadata(model_name=model_name, model_version=model_version)
-    model_config = triton_client.get_model_config(model_name=model_name, model_version=model_version)
+    model_metadata = triton_client.get_model_metadata(model_name=model_name, model_version=args.model_version)
+    model_config = triton_client.get_model_config(model_name=model_name, model_version=args.model_version)
 
-    question_input = tritonclient.http.InferInput(name="QUESTION", shape=(batch_size,), datatype="BYTES")
-    context_input = tritonclient.http.InferInput(name="CONTEXT", shape=(batch_size,), datatype="BYTES")
-    output = tritonclient.http.InferInput(name="output", shape=(batch_size,), datatype="BYTES")
+    question_input = tritonclient.http.InferInput(name="QUESTION", shape=(args.batch_size,), datatype="BYTES")
+    context_input = tritonclient.http.InferInput(name="CONTEXT", shape=(args.batch_size,), datatype="BYTES")
+    output = tritonclient.http.InferInput(name="output", shape=(args.batch_size,), datatype="BYTES")
 
-    question_input.set_data_from_numpy(np.asarray([question] * batch_size, dtype=object))
-    context_input.set_data_from_numpy(np.asarray([text] * batch_size, dtype=object))
+    question_input.set_data_from_numpy(np.asarray([question] * args.batch_size, dtype=object))
+    context_input.set_data_from_numpy(np.asarray([text] * args.batch_size, dtype=object))
     response = triton_client.infer(
         model_name=model_name,
-        model_version=model_version,
+        model_version=args.model_version,
         inputs=[question_input, context_input],
         outputs=[output],
     )
